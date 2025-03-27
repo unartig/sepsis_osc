e1436da0e836f9ff06bac9c119a214b16556bab5
tinkpad <jurinho@hotmail.de>
2025-03-27 13:21:30 +0100

    new directory structure

---
+++ utils/config.py
@@ -0,0 +1,17 @@
+# Random seeds
+jax_random_seed = 123
+np_random_seed = 123
+random_seed = 123
+
+# DB names
+db_parameter_keys = "storage/SepsisParameters_index.bin"
+db_metrics_key_value = "storage/SepsisMetrics.db"
+
+# Log Config
+log_level = "info"
+log_file = "sepsis.log"
+
+# TODO
+# Merging of two Rocks into a stone
+# FAISS  ranges for parameters, so we can normalize
+# threshold distance for retrieval
