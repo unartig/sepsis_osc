8db2cfbc19e5bdf94f951a1e9eddda05b13b39d2
tinkpad <jurinho@hotmail.de>
2025-05-01 11:25:54 +0200

    added transient time detection

--- utils/viz_ensemble_run.py
+++ utils/viz_ensemble_run.py
@@ -9,6 +9,7 @@ def plot_metric_t(var_1: np.ndarray, var_2: np.ndarray, ax=None):
     if not ax:
         _, ax = plt.subplots(2, 1)
     t = np.arange(var_1.shape[0])
+    t = t / t.max() * run_conf.T_max
     ax[0].plot(t, var_1)
     ax[1].plot(t, var_2)
     return ax
@@ -17,7 +18,7 @@ def plot_metric_t(var_1: np.ndarray, var_2: np.ndarray, ax=None):
 if __name__ == "__main__":
     import jax.numpy as jnp
     import jax.random as jr
-    from diffrax import Bosh3, Dopri5, Dopri8, ODETerm, PIDController
+    from diffrax import Bosh3, Dopri5, Tsit5, Dopri8, ODETerm, PIDController
     from jax import vmap
 
     from utils.run_simulation import solve
@@ -29,15 +30,15 @@ if __name__ == "__main__":
     from utils.config import jax_random_seed
 
     rand_key = jr.key(jax_random_seed + 123)
-    num_parallel_runs = 100
+    num_parallel_runs = 50
     rand_keys = jr.split(rand_key, num_parallel_runs)
-    full_save = make_metric_save(system_deriv)
+    metric_save = make_metric_save(system_deriv)
     term = ODETerm(system_deriv)
-    solver = Dopri5()
-    stepsize_controller = PIDController(rtol=1e-4, atol=1e-7, dtmax=0.1)
+    solver = Tsit5()
+    stepsize_controller = PIDController(rtol=1e-4, atol=1e-7)
 
     #### Parameters
-    N = 200
+    N = 100
     run_conf = SystemConfig(
         N=N,
         C=int(0.2 * N),  # local infection
@@ -51,7 +52,7 @@ if __name__ == "__main__":
         sigma=1.1,
         T_init=0,
         T_trans=0,
-        T_max=25,
+        T_max=1500,
         T_step=0.1,
     )
     generate_init_conditions = generate_init_conditions_fixed(run_conf.N, run_conf.beta, run_conf.C)
@@ -67,22 +68,36 @@ if __name__ == "__main__":
         solver,
         term,
         stepsize_controller,
-        full_save,
+        metric_save,
     )
-    if not sol.ys:
+    if not sol.ys or not run_conf.T_max:
         exit(0)
     metrics = sol.ys
     metrics = metrics.add_follow_ups()
     ts = np.asarray(sol.ts).squeeze()
     ts = ts[~jnp.isinf(ts)]
-    print(metrics.f_1.shape)
-    print(metrics.s_1.shape)
-    print(metrics.r_1.shape)
+
+    # calculate transient time
+    last_x = metrics.r_1[-int(0.3 * run_conf.T_max) :]
+    tt = np.where(np.max(np.abs(metrics.r_1 - last_x.mean(axis=0)), axis=-1) > 0.05)[0]
+    print(tt)
+    tt = int(tt[-1] / metrics.r_1.shape[0] * run_conf.T_max)
+
     ax = plot_metric_t(metrics.r_1, metrics.r_2)
     ax[0].set_ylim([0, 1])
     ax[1].set_ylim([0, 1])
+    ax[0].vlines(tt, 0, 1, color="tab:red", ls=":")
+    ax[1].vlines(tt, 0, 1, color="tab:red", ls=":")
     ax[0].set_title("Kuramoto Order Parameter\nParenchymal Layer")
     ax[1].set_title("Immune Layer")
-    plt.tight_layout()
 
+    ax = plot_metric_t(metrics.s_1, metrics.s_2)
+    ax[0].vlines(tt, 0, 1, color="tab:red", ls=":")
+    ax[1].vlines(tt, 0, 1, color="tab:red", ls=":")
+    ax[0].set_ylim([0, 1])
+    ax[1].set_ylim([0, 1])
+    ax[0].set_title("Mean Phase Velocity Std\nParenchymal Layer")
+    ax[1].set_title("Immune Layer")
+
+    plt.tight_layout()
     plt.show()
