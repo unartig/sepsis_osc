4a4b4463d2f1c8639b86edc68cdfa4fd7efa21b3
tinkpad <jurinho@hotmail.de>
2025-05-20 15:53:24 +0200

    classification to regression

--- src/sepsis_osc/model/data_loading.py
+++ src/sepsis_osc/model/data_loading.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from icu_benchmarks.data.split_process_data import preprocess_data
 from icu_benchmarks.constants import RunMode
+from icu_benchmarks.data.preprocessor import PolarsRegressionPreprocessor
 
 from sepsis_osc.utils.logger import setup_logging
 from sepsis_osc.utils.config import yaib_data_dir
@@ -16,21 +17,21 @@ data_dir = Path(yaib_data_dir)
 
 print(os.listdir(data_dir))
 
-
 data = preprocess_data(
     data_dir=data_dir,
     file_names=file_names,
+    preprocessor=PolarsRegressionPreprocessor,
     seed=666,
     debug=False,
-    load_cache=True,
     generate_cache=True,
+    load_cache=True,
     cv_repetitions=5,
     repetition_index=0,
     train_size=None,
     cv_folds=5,
     fold_index=0,
     pretrained_imputation_model=None,
-    runmode=RunMode.classification,
+    runmode=RunMode.regression,
     vars=vars,
     modality_mapping=modality_mapping,
 )
