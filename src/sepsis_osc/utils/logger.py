b0c9a37fedcecc1b48889b982c68abe7d4ba280c
tinkpad <jurinho@hotmail.de>
2025-05-27 09:30:04 +0200

    minor logger changes

--- src/sepsis_osc/utils/logger.py
+++ src/sepsis_osc/utils/logger.py
@@ -15,28 +15,31 @@ def setup_logging(log_level: str = cfg_log_level, console_log=True):
     else:
         level = logging.WARNING
 
-    logger = logging.getLogger("SepOsc_logger")
-    if logger.handlers:
-        logger.warning("Logging is already configured for 'my_app'. Skipping setup.")
-        return
+    # logger = logging.getLogger("sepsis_osc")
+    # if logger.handlers:
+    #     logger.warning("Logging is already configured. Skipping setup.")
+    #     return
 
+    logger = logging.getLogger()
     if console_log:
-        console_formatter = colorlog.ColoredFormatter(
-            "%(asctime)s - %(name)-30s - %(log_color)s%(levelname)-8s%(reset)s - %(message)s",
-            datefmt="%Y-%m-%d %H:%M:%S",
-            log_colors={
-                "DEBUG": "cyan",
-                "INFO": "green",
-                "WARNING": "yellow",
-                "ERROR": "red",
-                "CRITICAL": "magenta",
-            },
-        )
-        console_handler = logging.StreamHandler()
-        console_handler.setFormatter(console_formatter)
+        if not any(isinstance(handler, logging.StreamHandler) for handler in logger.handlers):
+            console_formatter = colorlog.ColoredFormatter(
+                "%(asctime)s - %(name)-30s - %(log_color)s%(levelname)-8s%(reset)s - %(message)s",
+                datefmt="%Y-%m-%d %H:%M:%S",
+                log_colors={
+                    "DEBUG": "cyan",
+                    "INFO": "green",
+                    "WARNING": "yellow",
+                    "ERROR": "red",
+                    "CRITICAL": "magenta",
+                },
+            )
+            console_handler = logging.StreamHandler()
+            console_handler.setFormatter(console_formatter)
 
-        logger.setLevel(level)
-        logger.addHandler(console_handler)
+            logger.setLevel(level)
+            logger.addHandler(console_handler)
+            logger.propagate = False
 
     if log_file:
         file_formatter = logging.Formatter(
