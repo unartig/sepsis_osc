f5160ab34588094f00146c79b646e56ea346f797
tinkpad <jurinho@hotmail.de>
2025-06-17 14:12:23 +0200

    Added model utils

--- src/sepsis_osc/model/vae.py
+++ src/sepsis_osc/model/vae.py
@@ -1,6 +1,4 @@
-import json
 import logging
-import os
 
 import equinox as eqx
 import jax
@@ -19,7 +17,7 @@ logger = logging.getLogger(__name__)
 # === Model Definitions ===
 LATENT_DIM = 3
 INPUT_DIM = 52
-ENC_HIDDEN = 64
+ENC_HIDDEN = 32
 DEC_HIDDEN = 32
 
 
@@ -33,6 +31,8 @@ class Encoder(eqx.Module):
     beta_layer: eqx.nn.Linear
     sigma_layer: eqx.nn.Linear
 
+    temperature_layer: eqx.nn.Linear
+
     input_dim: int
     latent_dim: int
     enc_hidden: int
@@ -44,9 +44,11 @@ class Encoder(eqx.Module):
         input_dim: int = INPUT_DIM,
         latent_dim: int = LATENT_DIM,
         enc_hidden: int = ENC_HIDDEN,
-        dropout_rate: float = 0.3,
+        dropout_rate: float = 0.5,
     ):
-        key1, key2, key_attn_score, key3, key4, key_alpha, key_beta, key_sigma, _ = jax.random.split(key, 9)
+        key1, key2, key_attn_score, key3, key4, key_alpha, key_beta, key_sigma, key_temp = (
+            jax.random.split(key, 9)
+        )
 
         self.input_dim = input_dim
         self.latent_dim = latent_dim
@@ -75,6 +77,8 @@ class Encoder(eqx.Module):
         self.beta_layer = eqx.nn.Linear(in_features=latent_dim, out_features=1, key=key_beta)
         self.sigma_layer = eqx.nn.Linear(in_features=latent_dim, out_features=1, key=key_sigma)
 
+        self.temperature_layer = eqx.nn.Linear(latent_dim, out_features=1, key=key_temp)
+
     def __call__(self, x: Float[Array, "input_dim"], *, key):
         x_original = x
 
@@ -94,26 +98,26 @@ class Encoder(eqx.Module):
         for layer in self.final_layers:
             x_final_enc = layer(x_final_enc)
 
-        alpha_raw = self.alpha_layer(x_final_enc)
-        beta_raw = self.beta_layer(x_final_enc)
-        sigma_raw = self.sigma_layer(x_final_enc)
+        alpha_raw = jax.nn.sigmoid(self.alpha_layer(x_final_enc))
+        beta_raw = jax.nn.sigmoid(self.beta_layer(x_final_enc))
+        sigma_raw = jax.nn.sigmoid(self.sigma_layer(x_final_enc))
 
-        return jax.nn.sigmoid(alpha_raw), jax.nn.sigmoid(beta_raw), jax.nn.sigmoid(sigma_raw)
+        temperature = (jax.nn.sigmoid(self.temperature_layer(x_final_enc)) + 1e-4) * 3
+
+        return alpha_raw, beta_raw, sigma_raw, temperature
 
 
 @eqx.filter_jit
 def get_pred_concepts(
-    z: Float[Array, "batch_size latent_dim"], lookup_table: JAXLookup
-) -> Float[Array, "batch_size 2"]:
-    # z = jax.lax.stop_gradient(z)
-    # sim_results: SystemMetrics = lookup_table.get(z, threshold=0.5)
-    sim_results: SystemMetrics = lookup_table.soft_get(z, temperature=0.1)
+    z: Float[Array, "batch latent"],
+    temperatures: Float[Array, "batch 1"],
+    *,
+    lookup_table: JAXLookup,
+) -> Float[Array, "2"]:
+    # pred_c: Float[Array, "2"] = jax.vmap(lookup_table.soft_get)(z, temperatures=temperatures)
+    pred_c: Float[Array, "batch 2"] = lookup_table.hard_get(z, temperatures=temperatures)
 
-    # Extract f_1 and sr_2 + f_2 from the JAXSystemMetrics
-    # SOFA and infection
-    pred_c = jnp.array([sim_results.f_1, jnp.clip(sim_results.sr_2 + sim_results.f_2, 0, 1)])
-
-    return pred_c.squeeze().T
+    return pred_c
 
 
 class Decoder(eqx.Module):
@@ -136,7 +140,7 @@ class Decoder(eqx.Module):
             jax.nn.softplus,
         ]
 
-    def __call__(self, z: Float[Array, "latent_dim"]) -> Float[Array, "input_dim"]:
+    def __call__(self, z: Float[Array, "batch latent_dim"]) -> Float[Array, "batch input_dim"]:
         z = z.reshape(
             self.latent_dim,
         )
@@ -218,79 +222,10 @@ def init_decoder_weights(decoder: Decoder, key: jnp.ndarray):
 
 
 def make_encoder(key, input_dim: int, latent_dim: int, enc_hidden: int, dropout_rate: float):
-    return Encoder(key, input_dim, latent_dim, enc_hidden)
+    return Encoder(key, input_dim, latent_dim, enc_hidden, dropout_rate=dropout_rate)
 
 
 def make_decoder(key, input_dim: int, latent_dim: int, dec_hidden: int):
     return Decoder(key, input_dim, latent_dim, dec_hidden)
 
 
-def save_checkpoint(
-    save_dir, epoch, params_enc, static_enc, params_dec, static_dec, opt_state_enc, opt_state_dec, hyper_enc, hyper_dec
-):
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-
-    filename = os.path.join(save_dir, f"checkpoint_epoch_{epoch:04d}.eqx")
-
-    full_model_state = {
-        "encoder_params": params_enc,
-        "encoder_static": static_enc,
-        "decoder_params": params_dec,
-        "decoder_static": static_dec,
-        "opt_state_enc": opt_state_enc,
-        "opt_state_dec": opt_state_dec,
-    }
-    hyper = {"encoder": hyper_enc, "decoder": hyper_dec}
-
-    with open(filename, "wb") as f:
-        hyperparam_str = json.dumps(hyper)
-        f.write((hyperparam_str + "\n").encode())
-        eqx.tree_serialise_leaves(f, full_model_state)
-    logger.info(f"Model checkpoint saved for epoch {epoch} to {filename}")
-
-
-def load_checkpoint(load_dir, epoch, opt_enc_template=None, opt_dec_template=None):
-    filename = os.path.join(load_dir, f"checkpoint_epoch_{epoch:04d}.eqx")
-
-    if not os.path.exists(filename):
-        raise FileNotFoundError(f"Checkpoint for epoch {epoch} not found at {filename}")
-
-    with open(filename, "rb") as f:
-        hyper = json.loads(f.readline().decode())
-        hyper_enc = hyper["encoder"]
-        hyper_dec = hyper["decoder"]
-
-        key_dummy = jax.random.PRNGKey(0)
-        skeleton_encoder = make_encoder(key_dummy, **hyper_enc)
-        skeleton_decoder = make_decoder(key_dummy, **hyper_dec)
-
-        skeleton_params_enc, skeleton_static_enc = eqx.partition(skeleton_encoder, eqx.is_array)
-        skeleton_params_dec, skeleton_static_dec = eqx.partition(skeleton_decoder, eqx.is_array)
-
-        if opt_enc_template is None or opt_dec_template is None:
-            logger.error("Reading empty model templates")
-        skeleton_opt_state_enc = opt_enc_template.init(skeleton_params_enc) if opt_enc_template else None
-        skeleton_opt_state_dec = opt_dec_template.init(skeleton_params_dec) if opt_dec_template else None
-
-        skeleton_full_model_state = {
-            "encoder_params": skeleton_params_enc,
-            "encoder_static": skeleton_static_enc,
-            "decoder_params": skeleton_params_dec,
-            "decoder_static": skeleton_static_dec,
-            "opt_state_enc": skeleton_opt_state_enc,
-            "opt_state_dec": skeleton_opt_state_dec,
-        }
-
-        full_model_state = eqx.tree_deserialise_leaves(f, skeleton_full_model_state)
-
-    logger.info(f"Model checkpoint loaded for epoch {epoch} from {filename}")
-
-    return (
-        full_model_state["encoder_params"],
-        full_model_state["encoder_static"],
-        full_model_state["decoder_params"],
-        full_model_state["decoder_static"],
-        full_model_state["opt_state_enc"],
-        full_model_state["opt_state_dec"],
-    )
