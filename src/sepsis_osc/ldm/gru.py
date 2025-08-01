ecb6dfdbf2438b40a114894b5a90d88655e590ab
tinkpad <jurinho@hotmail.de>
2025-08-01 10:26:25 +0200

    Dropout in GRU

--- src/sepsis_osc/ldm/gru.py
+++ src/sepsis_osc/ldm/gru.py
@@ -1,29 +1,41 @@
 import equinox as eqx
-from jaxtyping import Float, Array
+import jax
+import jax.numpy as jnp
 import jax.random as jr
+from beartype import beartype as typechecker
+from jaxtyping import Array, Float, jaxtyped
 
 
 class GRUPredictor(eqx.Module):
     gru_cell: eqx.nn.GRUCell
     proj_out: eqx.nn.Linear
+    dropout: eqx.nn.Dropout
 
     hidden_dim: int = eqx.field(static=True)
+    dropout_rate: float = eqx.field(static=True)
 
-    def __init__(self, key, dim: int, hidden_dim: int):
-        key1, key2 = jr.split(key)
+    def __init__(self, key, dim: int, hidden_dim: int, dropout_rate: float = 0.4, dtype=jnp.float32):
+        key1, key2, key3 = jr.split(key, 3)
         self.hidden_dim = hidden_dim
+        self.dropout_rate = dropout_rate
 
-        self.gru_cell = eqx.nn.GRUCell(dim, hidden_dim, key=key1)
-        self.proj_out = eqx.nn.Linear(hidden_dim, dim, key=key2)
+        self.gru_cell = eqx.nn.GRUCell(dim, hidden_dim, key=key1, dtype=dtype)
+        self.proj_out = eqx.nn.Linear(hidden_dim, dim, key=key2, dtype=dtype)
+        self.dropout = eqx.nn.Dropout(dropout_rate)
 
+    @jaxtyped(typechecker=typechecker)
     def __call__(
         self,
-        z_t: Float[Array, " latent_dim"],
-        h_prev: Float[Array, "hidden_dim"],
-    ) -> tuple[Float[Array, " latent_dim"], Float[Array, " hidden"]]:
+        z_t: Float[Array, " latent_dim_minus_1"],
+        h_prev: Float[Array, " pred_hidden"],
+        *,
+        key,
+    ) -> tuple[Float[Array, " latent_dim_minus_1"], Float[Array, " pred_hidden"]]:
         h_next = self.gru_cell(z_t, h_prev)
-        z_pred = self.proj_out(h_next)
+        h_next_dropped = self.dropout(h_next, key=key)
+        z_pred = jax.nn.tanh(self.proj_out(h_next_dropped))
         return z_pred, h_next
 
-def make_predictor(key, dim, hidden_dim):
-    return GRUPredictor(key, dim, hidden_dim)
+
+def make_predictor(key, dim, hidden_dim, dropout_rate):
+    return GRUPredictor(key, dim, hidden_dim, dropout_rate)
