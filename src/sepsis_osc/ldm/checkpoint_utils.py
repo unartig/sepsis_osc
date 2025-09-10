bc0b5e08fd2d450a2363f6102921ba81d9316636
tinkpad <jurinho@hotmail.de>
2025-09-10 16:56:41 +0200

    Restructure model_utils to avoid circular imports

---
+++ src/sepsis_osc/ldm/checkpoint_utils.py
@@ -0,0 +1,72 @@
+import json
+import logging
+import os
+
+import equinox as eqx
+import jax.random as jr
+from jaxtyping import PyTree
+from optax import GradientTransformation, OptState
+
+from sepsis_osc.ldm.ae import make_decoder, make_encoder
+from sepsis_osc.ldm.latent_dynamics import LatentDynamicsModel
+
+logger = logging.getLogger(__name__)
+
+
+
+def save_checkpoint(
+    save_dir: str,
+    epoch: int,
+    model,
+    opt_state: OptState,
+    hyper_enc: dict[str, float | int],
+    hyper_dec: dict[str, int],
+    hyper_pred: dict[str, int],
+):
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    filename = os.path.join(save_dir, f"checkpoint_epoch_{epoch:04d}.eqx")
+
+    # = {"opt_state": opt_state}
+    hyper = {"encoder": hyper_enc, "decoder": hyper_dec, "predictor": hyper_pred}
+
+    with open(filename, "wb") as f:
+        hyperparam_str = json.dumps(hyper)
+        f.write((hyperparam_str + "\n").encode())
+        eqx.tree_serialise_leaves(f, (model, opt_state))
+    logger.info(f"Model checkpoint saved for epoch {epoch} to {filename}")
+
+
+def load_checkpoint(
+    load_dir: str,
+    epoch: int,
+    opt_template: GradientTransformation | None = None,
+) -> tuple[PyTree, OptState]:
+    filename = os.path.join(load_dir, f"checkpoint_epoch_{epoch:04d}.eqx")
+
+    if not os.path.exists(filename):
+        raise FileNotFoundError(f"Checkpoint for epoch {epoch} not found at {filename}")
+
+    with open(filename, "rb") as f:
+        hyper = json.loads(f.readline().decode())
+        hyper_enc = hyper["encoder"]
+        hyper_dec = hyper["decoder"]
+        hyper_predictor = hyper["predictor"]
+
+        # Build skeleton full model
+        key_dummy = jr.PRNGKey(0)
+        encoder = make_encoder(key_dummy, **hyper_enc)
+        decoder = make_decoder(key_dummy, **hyper_dec)
+        predictor = make_predictor(key_dummy, **hyper_predictor)
+
+        model = eqx.nn.inference_mode(
+            LatentDynamicsModel(encoder=encoder, predictor=predictor, decoder=decoder), value=False
+        )
+        params_model, static_model = eqx.partition(model, eqx.is_array)
+
+        opt_state = opt_template.init(params_model) if opt_template else None
+
+        loaded_model, loaded_opt_state = eqx.tree_deserialise_leaves(f, (model, opt_state))
+    logger.info(f"Model checkpoint loaded for epoch {epoch} from {filename}")
+    return loaded_model, loaded_opt_state
---
+++ src/sepsis_osc/ldm/index_utils.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def as_3d_indices(
+    alpha_space: tuple[float, float, float],
+    beta_space: tuple[float, float, float],
+    sigma_space: tuple[float, float, float],
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    alphas = np.arange(*alpha_space)
+    betas = np.arange(*beta_space)
+    sigmas = np.arange(*sigma_space)
+    alpha_grid, beta_grid, sigma_grid = np.meshgrid(alphas, betas, sigmas, indexing="ij")
+    permutations = np.stack([alpha_grid, beta_grid, sigma_grid], axis=-1)
+    a, b, s = permutations[:, :, :, 0:1], permutations[:, :, :, 1:2], permutations[:, :, :, 2:3]
+    return a, b, s
+
+
+def as_2d_indices(
+    x_space: tuple[float, float, float],
+    y_space: tuple[float, float, float],
+) -> tuple[np.ndarray, np.ndarray]:
+    xs = np.arange(*x_space)
+    ys = np.arange(*y_space)
+    x_grid, y_grid = np.meshgrid(xs, ys, indexing="ij")
+    return x_grid, y_grid
+
+
+def flatten_dict(d, parent_key="", sep="_"):
+    return (
+        {
+            f"{k}" if parent_key else k: v
+            for pk, pv in d.items()
+            for k, v in flatten_dict(pv, f"{parent_key}{sep}{pk}" if parent_key else pk, sep).items()
+        }
+        if isinstance(d, dict)
+        else {parent_key: d}
+    )
