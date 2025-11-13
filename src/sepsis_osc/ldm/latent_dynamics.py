dceb874c157c3c97fdd12fa60398b405fb1af02c
tinkpad <jurinho@hotmail.de>
2025-11-13 12:52:56 +0100

    More LDM cleanup

--- src/sepsis_osc/ldm/latent_dynamics.py
+++ src/sepsis_osc/ldm/latent_dynamics.py
@@ -6,8 +6,8 @@ from jaxtyping import Array, Float
 from sepsis_osc.ldm.ae import Decoder, Encoder
 from sepsis_osc.ldm.gru import GRUPredictor
 
-ones_24 = jnp.ones((24))
-ones_25 = jnp.ones((25))
+ones_24 = jnp.ones(24)
+ones_25 = jnp.ones(25)
 
 
 class LatentDynamicsModel(eqx.Module):
@@ -20,25 +20,13 @@ class LatentDynamicsModel(eqx.Module):
 
     _label_temperature: Array
     _lookup_temperature: Array
-    _delta_temperature: Array
     _prior_deltas: Array = eqx.field(static=True)
-    _learned_deltas: Array
 
     _alpha: float = eqx.field(static=True)
 
     _d_diff: Array
     _d_scale: Array
     _input_sim_scale: Array
-
-    _sofa_exp: Array
-
-    _inf_w: Array
-    _inf_b: Array
-    _inf_p: Array
-
-    _sofa_w: Array
-    _sofa_b: Array
-    _sofa_p: Array
 
     _sofa_dir_lsigma: Array
     _sofa_d2_lsigma: Array
@@ -69,21 +57,11 @@ class LatentDynamicsModel(eqx.Module):
 
         self._label_temperature = jnp.log(jnp.ones((1,), dtype=jnp.float32) * 0.05)
         self._lookup_temperature = jnp.log(jnp.ones((1,), dtype=jnp.float32) * 0.05)
-        self._delta_temperature = jnp.log(jnp.ones((1,), dtype=jnp.float32) * 0.5)
         self._prior_deltas = ordinal_deltas
-        self._learned_deltas = ordinal_deltas
 
         self._d_diff = jnp.log(jnp.ones((1,), dtype=jnp.float32) * 1 / 25)
         self._d_scale = jnp.log(jnp.ones((1,), dtype=jnp.float32))
-        self._sofa_exp = jnp.zeros((1,), dtype=jnp.float32)
         self._input_sim_scale = jnp.ones((encoder.input_dim,), dtype=jnp.float32)
-
-        self._inf_w = jnp.zeros((1,), dtype=jnp.float32)
-        self._inf_b = jnp.zeros((1,), dtype=jnp.float32)
-        self._inf_p = jnp.zeros((1,), dtype=jnp.float32)
-        self._sofa_w = jnp.ones((1,), dtype=jnp.float32)
-        self._sofa_b = jnp.zeros((1,), dtype=jnp.float32)
-        self._sofa_p = jnp.zeros((1,), dtype=jnp.float32)
 
         self._sofa_dir_lsigma = jnp.zeros((1,), dtype=jnp.float32)
         self._sofa_d2_lsigma = jnp.zeros((1,), dtype=jnp.float32)
@@ -100,19 +78,12 @@ class LatentDynamicsModel(eqx.Module):
         return sum(x.size if isinstance(x, jnp.ndarray) else 1 for x in jax.tree_util.tree_leaves(self))
 
     @property
-    def learned_deltas(self) -> Float[Array, "25"]:
-        return self._learned_deltas
-
-    @property
     def alpha(self) -> float:
         return self._alpha
 
     @property
     def sofa_dist(self) -> Float[Array, "24"]:
         return self._sofa_dist
-    @property
-    def sofa_exp(self) -> Float[Array, "1"]:
-        return jnp.exp(self._sofa_exp)
 
     @property
     def label_temperature(self) -> Float[Array, "1"]:
@@ -123,19 +94,9 @@ class LatentDynamicsModel(eqx.Module):
         return jnp.exp(self._lookup_temperature)
 
     @property
-    def delta_temperature(self) -> Float[Array, "1"]:
-        return jnp.exp(self._delta_temperature)
-
-    def ordinal_thresholds(self, lam: Float[Array, "1"]) -> Float[Array, "24"]:
-        combined_deltas = lam * self._learned_deltas + (1.0 - lam) * self._prior_deltas
-
+    def ordinal_thresholds(self) -> Float[Array, "24"]:
+        combined_deltas = self._prior_deltas
         return jnp.cumsum(jax.nn.softmax(combined_deltas))[:-1]
-
-    def transform_infs(self, pred_infs: Float[Array, "*"]) -> Float[Array, "*"]:
-        return jax.nn.sigmoid(((pred_infs + self._inf_b) ** jnp.exp(self._inf_p)) * self._inf_w)
-
-    def transform_sofas(self, pred_sofas: Float[Array, "*"]) -> Float[Array, "*"]:
-        return jax.nn.sigmoid((pred_sofas + self._sofa_b) * self._sofa_w)
 
     @property
     def input_sim_scale(self) -> Float[Array, " input_dim"]:
@@ -189,7 +150,6 @@ class LatentDynamicsModel(eqx.Module):
         return {
             "label_temperature": self.label_temperature,
             "lookup_temperature": self.lookup_temperature,
-            "delta_temperature": self.delta_temperature,
             "d_diff": self.d_diff,
             "d_scale": self.d_scale,
             "sofa_lsigma": self.sofa_class_lsigma,
