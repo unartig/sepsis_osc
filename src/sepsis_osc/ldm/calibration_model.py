6db39a128b544fd10af71a8beb8a3c3b88921a83
tinkpad <jurinho@hotmail.de>
2025-12-17 09:59:32 +0100

    Only Platt scaling for real

--- src/sepsis_osc/ldm/calibration_model.py
+++ src/sepsis_osc/ldm/calibration_model.py
@@ -13,11 +13,6 @@ arr_0 = jnp.array(0)
 
 
 class CalibrationModel(eqx.Module):
-    # Interaction Model
-    beta0: Float[Array, ""]
-    beta1: Float[Array, ""]
-    beta2: Float[Array, ""]
-    beta12: Float[Array, ""]
 
     # Calibration
     a: Float[Array, ""]
@@ -25,55 +20,35 @@ class CalibrationModel(eqx.Module):
 
     def __init__(
         self,
-        beta0: Float[Array, ""] = arr_0,
-        beta1: Float[Array, ""] = arr_0,
-        beta2: Float[Array, ""] = arr_0,
-        beta12: Float[Array, ""] = arr_0,
         a: Float[Array, ""] = arr_1,
         b: Float[Array, ""] = arr_0,
     ) -> None:
-        self.beta0 = beta0
-        self.beta1 = beta1
-        self.beta2 = beta2
-        self.beta12 = beta12
         self.a = a
         self.b = b
 
     @property
-    def betas(self) -> Float[Array, ""]:
-        return jnp.array([self.beta0, self.beta1, self.beta2, self.beta12])
+    def coeffs(self) -> Float[Array, ""]:
+        return jnp.array([self.a, self.b])
 
     @jaxtyped(typechecker=typechecker)
     def get_sepsis_logits(
         self,
-        sofa: Float[Array, " samples"] | Float[Array, " batch time"],
-        inf: Float[Array, " samples"] | Float[Array, " batch time"],
-        betas: Float[Array, "4"] | None = None,
-    ) -> Float[Array, " samples"] | Float[Array, " batch time"]:
-        betas = betas if betas is not None else self.betas
-        X = jnp.stack(
-            [
-                jnp.ones_like(sofa),  # intercept
-                sofa,  # sofa
-                inf,  # infection
-                sofa * inf,  # interaction
-            ],
-            axis=-1,
-        )
-        return jnp.tensordot(X, betas, axes=[-1, 0])
+        sofa: Float[Array, " samples"] | Float[Array, " batch time"] | Float[Array, "*"],
+        inf: Float[Array, " samples"] | Float[Array, " batch time"] | Float[Array, "*"],
+    ) -> Float[Array, " samples"] | Float[Array, " batch time"] | Float[Array, "*"]:
+        return sofa * inf  # interaction
 
     @jaxtyped(typechecker=typechecker)
     def get_sepsis_probs(
         self,
-        p_sofa: Float[Array, " samples"] | Float[Array, " batch time"],
-        p_inf: Float[Array, " samples"] | Float[Array, " batch time"],
-        betas: Float[Array, "4"] | None = None,
+        p_sofa: Float[Array, " samples"] | Float[Array, " batch time"] | Float[Array, "*"],
+        p_inf: Float[Array, " samples"] | Float[Array, " batch time"] | Float[Array, "*"],
         a: Float[Array, ""] | None = None,
         b: Float[Array, ""] | None = None,
-    ) -> Float[Array, " samples"] | Float[Array, " batch time"]:
+    ) -> Float[Array, " samples"] | Float[Array, " batch time"] | Float[Array, "*"]:
         a = a if a is not None else self.a
         b = b if b is not None else self.b
-        return jax.nn.sigmoid(a * self.get_sepsis_logits(p_sofa, p_inf, betas) + b)
+        return jax.nn.sigmoid(a * self.get_sepsis_logits(p_sofa, p_inf) + b)
 
     @jaxtyped(typechecker=typechecker)
     @eqx.filter_jit
@@ -85,30 +60,16 @@ class CalibrationModel(eqx.Module):
     ) -> "CalibrationModel":
         solver = BFGS(rtol=1e-8, atol=1e-8)
 
-        def loss_fn_interaction(parameters: CalibrationModel, args: tuple[Float[Array, " samples"], ...]) -> ScalarLike:
-            p_sofa, p_inf, y = args
-            logits = self.get_sepsis_logits(p_sofa, p_inf, parameters.betas)
-            return jnp.mean(optax.sigmoid_binary_cross_entropy(logits, y.astype(jnp.float32)))
-
-        res = minimise(
-            fn=loss_fn_interaction,
-            solver=solver,
-            y0=self,  # initial parameter guess
-            args=(p_sofa, p_inf, p_sepsis),
-            throw=False,
-            max_steps=int(1e3),
-        )
-
         def loss_fn_calibration(parameters: CalibrationModel, args: tuple[Float[Array, " samples"], ...]) -> ScalarLike:
-            betas, p_sofa, p_inf, y = args
-            probs = self.get_sepsis_probs(p_sofa, p_inf, betas, parameters.a, parameters.b)
+            p_sofa, p_inf, y = args
+            probs = self.get_sepsis_probs(p_sofa, p_inf, parameters.a, parameters.b)
             return jnp.mean(optax.sigmoid_binary_cross_entropy(binary_logits(probs), y.astype(jnp.float32)))
 
         res = minimise(
             fn=loss_fn_calibration,
             solver=solver,
-            y0=res.value,
-            args=(res.value.betas, p_sofa, p_inf, p_sepsis),
+            y0=self,
+            args=(p_sofa, p_inf, p_sepsis),
             throw=False,
             max_steps=int(1e3),
         )
